4e60c8177d6fde21663bd1746f336ce23d1e0e1b
Roman Spiridonov <Rom@nSpiridonov.ru>
2023-04-03 08:41:01 +0300

    Added types update

--- index.d.ts
+++ index.d.ts
@@ -32,14 +32,31 @@ export type Pool = {
 	networkId: number;
 };
 
+export type StakingPool = {
+	address: string;
+	pool: {
+		coinX: string;
+		coinY: string;
+		curve: string;
+		networkId: number;
+	};
+	reward: string;
+	order?: number;
+	nftInfo?: {
+		url: string;
+	};
+};
+
 export type NetworkType = 'mainnet' | 'testnet';
 
 export type CoinsRegister = {
 	coins: Coin[];
 	pools: Pool[];
+	staking: Staking[];
 
 	getCoinsFor: (NetworkType) => Coin[];
 	getPoolsFor: (NetworkType) => Pool[];
+	getStakingPoolsFor: (NetworkType) => StakingPool[];
 };
 
 export = register;
